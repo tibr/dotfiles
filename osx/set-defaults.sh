0ae8acbf5214df4db2cf90d09446de62541ae0af
Tim Brückmann <tim@tibr.me>
2013-02-24 23:55:07 +0100

    Add mac os x trackpad defaults

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -3,3 +3,8 @@
 
 # Show the ~/Library folder.
 chflags nohidden ~/Library
+
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
