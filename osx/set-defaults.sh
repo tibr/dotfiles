3b881acd0e66d5fbb4836cdcff62f47fd2998a91
Tim Brückmann <tim@tibr.me>
2013-02-26 11:18:41 +0100

    Add Finder defaults

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -8,3 +8,10 @@ chflags nohidden ~/Library
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+# Show the home folder instead of all files when opening a new finder window
+defaults write com.apple.finder NewWindowTarget PfHm
