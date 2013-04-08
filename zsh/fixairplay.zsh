2dee88a087617170f55e309470577988924c89e1
Tim Brückmann <tim@tibr.me>
2013-04-08 09:40:16 +0200

    Add Airplay fix

---
+++ zsh/fixairplay.zsh
@@ -0,0 +1,4 @@
+# Quick way to restart the CoreAudio
+# Fixes problems with AirPlay after sleep mode
+# http://forums.macrumors.com/showthread.php?t=1412191
+alias fixairplay='sudo pkill coreaudiod'
