60825880260eec5babb3b407449c44cc20def95d
Tim Brückmann <tim@tibr.me>
2013-06-05 13:56:56 +0200

    Add commands to show/hide hidden files in finder

---
+++ zsh/hidden_files.zsh
@@ -0,0 +1,2 @@
+alias showhidden='defaults write com.apple.Finder AppleShowAllFiles YES; killall -HUP Finder'
+alias hidehidden='defaults write com.apple.Finder AppleShowAllFiles NO; killall -HUP Finder'
